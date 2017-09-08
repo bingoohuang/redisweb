a61c543e4c55ac2a17e927a80495dd2f71cf40b5
bingoo <bingoo.huang@gmail.com>
2017-09-08 11:37:05 +0800

    export downloadable

--- res/index.js
+++ res/index.js
@@ -42,26 +42,36 @@ $(function () {
         var contentHtml = '<div><h3>Export</h3></div>' +
             '<div><span style="margin-right: 30px">Type:</span>' +
             '<select style="margin-right: 30px" id="exportType"><option value="Redis">Redis</option><option value="JSON">JSON</option></select>' +
-            '<button id="exportBtn">Export</button></div>' +
+            '<button id="exportBtn">Export</button><button id="downloadExportBtn">Download</button></div>' +
             '<div id="exportResult"></div>'
 
         $('#frame').html(contentHtml)
 
-        $('#exportBtn').click(function () {
-            var keys = findCheckedKeys();
+        $('#exportBtn,#downloadExportBtn').click(function () {
+            var keys = findCheckedKeys()
             if (keys.length == 0) {
                 alert("No keys chosen to be deleted!")
                 return
             }
             var exportType = $('#exportType').val()
+            var download = $(this).prop('id') == 'downloadExportBtn'
+
+            var data = {
+                server: $('#servers').val(),
+                database: $('#databases').val(),
+                exportKeys: JSON.stringify(keys),
+                exportType: exportType,
+                download: download
+            }
+
+            if (download) {
+                window.open(pathname + "/exportKeys?" + $.param(data), '_blank')
+                return
+            }
+
             $.ajax({
                 type: 'GET', url: pathname + "/exportKeys",
-                data: {
-                    server: $('#servers').val(),
-                    database: $('#databases').val(),
-                    exportKeys: JSON.stringify(keys),
-                    exportType: exportType
-                },
+                data: data,
                 success: function (content, textStatus, request) {
                     if (exportType == "Redis") {
                         $('#exportResult').html('<pre>' + content.join('<br>') + '</pre>')
