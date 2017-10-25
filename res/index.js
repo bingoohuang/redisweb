8f23355ddcfa93b4c742e2e425f55badcb8cf8e6
bingoo <bingoo.huang@gmail.com>
2017-10-25 11:14:34 +0800

    paste as plain text

--- res/index.js
+++ res/index.js
@@ -811,5 +811,19 @@ $(function () {
         }
     })
 
+    $(document).on('paste','[contenteditable]', function(e) {
+        e.preventDefault();
+        var text = '';
+        if (e.clipboardData || e.originalEvent.clipboardData) {
+            text = (e.originalEvent || e).clipboardData.getData('text/plain');
+        } else if (window.clipboardData) {
+            text = window.clipboardData.getData('Text');
+        }
+        if (document.queryCommandSupported('insertText')) {
+            document.execCommand('insertText', false, text);
+        } else {
+            document.execCommand('paste', false, text);
+        }
+    });
 
 })
