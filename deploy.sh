ec359530460727e7d1fe03b957b48b33ee9c0033
bingoo <bingoo.huang@gmail.com>
2017-11-07 16:12:06 +0800

    deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# ./deploy.sh app@hd2.gw01 or ./deploy.sh app@hb2.gw01
+targetHost=$1
+./gobin.sh
+rm -fr go-redis-web.linux.bin go-redis-web.linux.bin.bz2
+env GOOS=linux GOARCH=amd64 go build -o go-redis-web.linux.bin
+bzip2 go-redis-web.linux.bin
+rsync -avz --human-readable --progress -e "ssh -p 22" ./go-redis-web.linux.bin.bz2 $targetHost:./
+#scp ./go-redis-web.linux.bin.bz2 $targetHost:./
+scp ./gw01.to.smc01.sh $targetHost:./
+ssh -tt $targetHost "bash -s" << eeooff
+chmod +x ./gw01.to.smc01.sh
+./gw01.to.smc01.sh
+rm -f ./gw01.to.smc01.sh
+exit
+eeooff
---
+++ gw01.to.smc01.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+scp ./go-redis-web.linux.bin.bz2 smc01:app/go-redis-web/
+ssh -tt smc01 "bash -s" << eeooff
+cd app/go-redis-web
+ps -ef|grep go-redis-web|grep -v grep|awk '{print \$2}'|xargs -r kill -9
+rm go-redis-web.linux.bin
+bzip2 -d go-redis-web.linux.bin.bz2
+nohup ./go-redis-web.linux.bin -servers=127.0.0.1:8051 > go-redis-web.out 2>&1 &
+exit
+eeooff
