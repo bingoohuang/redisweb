89fd865781e9fe781f3b44ba5c757e1ba7cefac0
bingoo <bingoo.huang@gmail.com>
2018-07-04 21:56:06 +0800

    update deploy-test.sh

--- deploy-test.sh
+++ deploy-test.sh
@@ -2,18 +2,23 @@
 
 # ./deploy.sh yogaapp@test.ino01
 targetHost=$1
-./gobin.sh
-rm -fr go-redis-web.linux.bin go-redis-web.linux.bin.bz2
-env GOOS=linux GOARCH=amd64 go build -o go-redis-web.linux.bin
-bzip2 go-redis-web.linux.bin
-rsync -avz --human-readable --progress -e "ssh -p 22" ./go-redis-web.linux.bin.bz2 $targetHost:./app/go-redis-web/
+deployName=go-redis-web
+fast=$2
+
+if [ "$fast" == "fast" ]; then
+    echo "jump building in fast mode"
+else
+    echo "rebuilding"
+    ./gobin.sh
+    env GOOS=linux GOARCH=amd64 go build -o $deployName.linux.bin
+    upx $deployName.linux.bin
+fi
+
+rsync -avz --human-readable --progress -e "ssh -p 22" ./$deployName.linux.bin $targetHost:.
 ssh -tt $targetHost "bash -s" << eeooff
-cd ./app/go-redis-web/
-ps -ef|grep go-redis-web|grep -v grep|awk '{print \$2}'|xargs -r kill -9
-rm go-redis-web.linux.bin
-bzip2 -d go-redis-web.linux.bin.bz2
-nohup ./go-redis-web.linux.bin -servers=127.0.0.1:8051 -devMode=true > go-redis-web.out 2>&1 &
+cd ./app/$deployNameb/
+ps -ef|grep $deployName|grep -v grep|awk '{print \$2}'|xargs -r kill -9
+mv -f ~/$deployName.linux.bin .
+nohup ./$deployName.linux.bin -servers=127.0.0.1:8051 > $deployName.out 2>&1 &
 exit
 eeooff
-
-rm -fr go-redis-web.linux.bin.bz2
